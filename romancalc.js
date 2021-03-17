73afb110fef5c87bc6d550d94a987988d8581975
Thiago Henrique Gomes <thiagoh.gomes23@gmail.com>
2021-03-16 21:56:51 -0300

    Second Week

--- romancalc.js
+++ romancalc.js
@@ -5,7 +5,43 @@ function get_args() // create the function that will store the input
 }
 	let op = get_args(); //the variable gets the array from the function
 
-	console.log("The first operand is: ", op[0]); // print the message and the value
-	console.log("The second operand is: ", op[2]);
-	console.log("The operator is: ", op[1]);	
+function calculate() //function that will calculate and show the resultes
+{
+	var a = op[0];  // the variable a gets whats in the first position of the array op
+	var b = op[1]; //the variable a gets whats in the second position of the array op
+	var c = op[2]; //the variable a gets whats in the third position of the array op
+	let resultado; //initialize the variable resultado
+		
+	if (c=="+"){ //if the variable c is the symbol for the operation then the resultado variable gets that a that operation b
+				resultado=Number(op[0])+Number(op[1]);
+				}
+	if (c=="-"){ 
+				resultado=a-b;
+				}
+	if (c=="x"){
+				resultado=a*b;
+				}
+	if (c=="/"){
+				resultado=a/b;
+				}
+	if (c=="^"){
+				resultado=a^b;
+				}	
+	if (c=="%"){
+				resultado=a%b;
+	}
 	
+	
+	return resultado; //the function returns the variable
+}
+	//console.log(calculate()); //print the function
+
+
+	console.log("The first operand is: ", op[0]); // print the message and the value
+	console.log("The second operand is: ", op[1]);
+	console.log("The operator is: ", op[2]);	
+	
+
+
+
+
